6206f5c49e5be12695e2ec4c9e02620ec69ba743
AshutoshSah-2000 <42838241+AshutoshSah-2000@users.noreply.github.com>
2019-06-26 22:14:38 +0400

    Add files via upload

--- merchant/models.py
+++ merchant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from user_auth.models import user_info
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Company(models.Model):
+    CompanyName = models.CharField(max_length=40)
+    CompanyGST = models.CharField(max_length=50)
+    Location = models.TextField(verbose_name="Company's base location")
+    Owner = models.ForeignKey(user_info,on_delete=models.CASCADE) # foreign key with merchant
+    PhoneNo = models.IntegerField(verbose_name="Company Phone No")
+    Email = models.EmailField()
+    
+
+    def __str__(self):
+        return self.CompanyName
+
+
+class TruckInfo(models.Model):
+    TruckName = models.CharField(max_length = 255)
+    TruckId = models.CharField(max_length=10,verbose_name="Number Plate of Truck")
+    PermitType = models.CharField(max_length = 25)
+    Company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    PermitDate = models.DateField(verbose_name="Till when is the permit valid")
+
+    def __str__(self):
+        return "Truck Name: {} ----- Truck Id: {} ".format(self.TruckName,self.TruckId)
+
+
+
--- merchant/views.py
+++ merchant/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
+from user_auth.models import user_info
 from django.contrib.auth import authenticate,login
 from user_auth.models import user_info
 from django.http import HttpResponse
+from .forms import CompanyForm,TruckForm
+from .models import Company,TruckInfo
+from django.views.decorators.csrf import csrf_exempt
+#from customer.models import order,order_bets,items,item_info,order_path_info,items
 
+def home_view(request):
+    
+    return render(request,'merchant/merchant_home.html')
+
+@csrf_exempt
 def login_view(request):
 
      if request.user.is_authenticated:
-        return render(request,'base.html',{})
+        return redirect("/merchant/")
 
      if request.method == 'POST':
         # First get the username and password supplied
@@ -25,12 +35,12 @@ def login_view(request):
             if user.is_active:
                 # Log the user in.
                 user_verification = user_info.objects.check_user(username)
-                if user_verification and user_verification[0].merchantOrUser=='merchant':
+                if user_verification and user_verification[0].merchant==True:
                 # Log the user in.
                     login(request,user)
                 # Send the user back to some page.
                 # In this case their homepage.
-                    return render(request,'base2.html',{})
+                    return redirect("/merchant/")
                 else:
                     return HttpResponse('Your account exists as user not merchant!!')
             else:
@@ -44,3 +54,55 @@ def login_view(request):
      else:
         #Nothing has been provided for username or password.
         return render(request, 'user_auth/login.html', {})
+
+
+@csrf_exempt
+def CompanyRegisterView(request):
+    if request.method=='POST':
+        companyForm = CompanyForm(data=request.POST)
+        if companyForm.is_valid():
+            company = companyForm.save()
+            company.save()
+            print(company)
+            return redirect("/merchant/")
+
+    else:
+        companyForm=CompanyForm
+    return render(request,'merchant/company.html',{'company':companyForm})
+
+
+@csrf_exempt
+def TruckRegisterView(request):
+    if request.method=='POST':
+        truckForm = TruckForm(data=request.POST)
+        if truckForm.is_valid():
+            t=truckForm.save()
+            t.save()
+            return redirect("/merchant/")
+    else:
+        truckForm = TruckForm
+    return render(request,'merchant/truck.html',{'truck':truckForm})
+
+
+
+def Profile(request):
+    current_user = request.user
+    u = user_info.objects.filter(merchant=True)
+
+    c = Company.objects.filter(Owner=u[0].id)
+    
+    t=[]
+    for i in c:
+        
+        temp = TruckInfo.objects.filter(Company=i.id)
+        print(temp.exists())
+        if temp.exists()==True:
+            print(temp)
+            t.append(temp)
+        print (t)
+    
+    return render(request,'merchant/profile.html',{'u':u,'c':c,'t':t})
+
+
+def AllAuctions(request):
+    pass

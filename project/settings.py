8faf5781f791ec3b8ec8dbb79ba67648a8c2af0f
AshutoshSah-2000 <42838241+AshutoshSah-2000@users.noreply.github.com>
2019-06-26 22:10:25 +0400

    Add files via upload

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user_auth',
     'customer',
-    'merchant'
+    'merchant',
+    'background_task',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from user_auth.views import home_view
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user_auth/',include("user_auth.urls")),
@@ -25,3 +25,5 @@ urlpatterns = [
     path('merchant/',include('merchant.urls')),
 
 ]
+
+urlpatterns +=staticfiles_urlpatterns()
